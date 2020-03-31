32ae820c630b2b739949ebe8d420e81c8503ba90
Twinkle Pardeshi <tpardesh@redhat.com>
2020-03-31 15:20:46 +0530

    Add login component file and aad it to app.js in routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 import "@patternfly/react-core/dist/styles/base.css";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-import AppPage from "./components/page";
 import Versions from "./components/Versions";
 import Products from "./components/Products";
 import Screenshots from "./components/Screenshots";
+import SimpleLoginPage from "./components/login_page";
 class PageLayoutSimpleNav extends React.Component {
   render() {
     return (
-      <AppPage>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/products" component={Products}></Route>
-            <Route
-              path="/products/:productid/product_versions"
-              component={Versions}
-            ></Route>
-            <Route path="/screenshots" component={Screenshots}></Route>
-          </Switch>
-        </BrowserRouter>
-      </AppPage>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={SimpleLoginPage}></Route>
+          <Route exact path="/products" component={Products}></Route>
+          <Route
+            path="/products/:productid/product_versions"
+            component={Versions}
+          ></Route>
+          <Route path="/screenshots" component={Screenshots}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
